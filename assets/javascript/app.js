4dc817d81b611921a81ead61f79e5a9c1a5c3860
matt babcock <babcockmatthew12@yahoo.com>
2018-03-15 15:52:36 -0700

    third commmit

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+
+
+theBoi.onclick= function(){
+    setTimeout()  {
+    
+}, 3000); 
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+<div class="btn-group btn-group-toggle" data-toggle="buttons">
+<label class="btn btn-secondary active">
+  <input type="radio" name="options" id="option1" autocomplete="off" checked> Active
+</label>
+<label class="btn btn-secondary">
+  <input type="radio" name="options" id="option2" autocomplete="off"> Radio
+</label>
+<label class="btn btn-secondary">
+  <input type="radio" name="options" id="option3" autocomplete="off"> Radio
+</label>
+</div>
